a8c8a12ed528c886a30164e571b04fd6f5b22a54
SajeevDarshanapu <67383521+SajeevDarshanapu@users.noreply.github.com>
2022-03-04 22:22:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace EvenorOdd
+{
+    public class EvenOdd
+    {
+        public static void Main(String[]args)
+        {
+            int num;
+            Console.WriteLine("Enter a Number:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if(num%2==0)
+            {
+                Console.WriteLine($"Given number {num} is even",num);
+            }
+            else
+            {
+                Console.WriteLine($"Given number {num} is odd",num );
+            }
+        }
+    }
+}
